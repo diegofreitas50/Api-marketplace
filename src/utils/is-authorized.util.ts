60f6fa3235bb124510cffec5825bced3b9ab4b59
diegofreitas50 <diegofreitas50@hotmail.com>
2022-06-28 22:56:39 -0300

    chore: user update

--- src/utils/is-authorized.util.ts
+++
@@ -1,8 +0,0 @@
-import { UnauthorizedException } from '@nestjs/common';
-import { User } from 'src/user/entities/user.entity';
-
-export function isAdmin(user: User, id: string) {
-  if (user.id === id) {
-    throw new UnauthorizedException('Você não tem permissão.');
-  }
-}
