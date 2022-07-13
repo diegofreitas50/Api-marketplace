4a7b760ca6ae32d74d0c13cdb25e7a5f499f9e9e
diegofreitas50 <diegofreitas50@hotmail.com>
2022-07-12 21:51:44 -0300

    fix: prisma

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
@@ -14,25 +14,24 @@ export class AuthService {
 
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
     const { email, password } = loginDto;
-    console.log(`dto.. ${email} - ${password}`);
 
     const user = await this.prisma.user.findUnique({ where: { email } });
 
     if (!user) {
-      throw new UnauthorizedException('Usuário e/ou senha inválidos');
+      throw new UnauthorizedException('Usuário e/ou senha inválidos.');
     }
 
     const isHashValid = await bcrypt.compare(password, user.password);
 
     if (!isHashValid) {
-      throw new UnauthorizedException('Usuário e/ou senha inválidos');
+      throw new UnauthorizedException('Usuário e/ou senha inválidos.');
     }
 
     delete user.password;
 
     return {
       token: this.jwtService.sign({ email }),
-      user: undefined,
+      user,
     };
   }
 }
